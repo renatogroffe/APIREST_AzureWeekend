6049b3b914362b61d272308e16ac19f8e0d1f44a
Renato Groffe <renato.groffe@yahoo.com.br>
2016-11-26 10:39:47 -0200

    Add project files.

---
+++ src/APIREST_AzureWeekend/Controllers/CotacoesController.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using APIREST_AzureWeekend.Data;
+
+namespace APIREST_AzureWeekend.Controllers
+{
+    [Route("api/[controller]")]
+    public class CotacoesController : Controller
+    {
+        [HttpGet("{id}")]
+        public Cotacao Get(
+            [FromServices]CotacoesContext context,
+            string id)
+        {
+            return context.Cotacoes
+                .Where(c => c.Sigla == id)
+                .FirstOrDefault();
+        }
+    }
+}
